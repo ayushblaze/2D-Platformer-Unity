e0d361295adace6499d97f57e4ba3dae52853e42
ayushblaze <ayushmoca@gmail.com>
2025-03-05 10:19:05 +0530

    fix(player-jump): prevent mid-air jump by adding ground check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float jumpForce;
 
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +23,12 @@ public class PlayerController : MonoBehaviour
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
 
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
         if (Input.GetButtonDown("Jump")) {
-            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            if (isGrounded) {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
         }
     }
 }
